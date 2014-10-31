ad5f7128b6cc5014926a8d7f89557653f259bc80
Santiago Garcia <santiagogarciabautista@gmail.com>
2014-10-31 13:00:32 -0500

    Quité espacios

--- mobile/Generics/Queryable.java
+++ mobile/Generics/Queryable.java
@@ -14,46 +14,34 @@ import mobile.DTO.Empleado;
 import oracle.adfmf.util.Utility;
 
 public class Queryable {
-    public  ResultSet query(GenericCriteria criteria)
-    {
-        
+    public ResultSet query(GenericCriteria criteria) {
         return doQuery(criteria.toString());
     }
-    
-    public  void insert(GenericCriteria criteria)
-    {
-         doQuery(criteria.getInsertQuery());
-        
+
+    public void insert(GenericCriteria criteria) {
+        doQuery(criteria.getInsertQuery());
     }
-    
-    public  void update(GenericCriteria criteria)
-    {
-        
+
+    public void update(GenericCriteria criteria) {
         doQuery(criteria.getUpdateQuery());
     }
-    
-    public  void delete(GenericCriteria criteria)
-    {
+
+    public void delete(GenericCriteria criteria) {
         doQuery(criteria.getDeleteQuery());
     }
-    
-    private  ResultSet doQuery(String query){
+
+    private ResultSet doQuery(String query) {
         Connection conn = null;
-       
-            ResultSet result = null;
+        ResultSet result = null;
         try {
             conn = ConnectionFactory.getConnection();
             Statement stmt = conn.createStatement();
             result = stmt.executeQuery(query);
-
         } catch (Exception ex) {
             Utility.ApplicationLogger.severe(ex.getMessage());
             ex.printStackTrace();
             throw new RuntimeException(ex);
         }
         return result;
-
     }
-    
-    
 }
